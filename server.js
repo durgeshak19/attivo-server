c06fe040798d24ae5708efdeabb6be9acb8d430f
Durgesh Kumar <durgesh.ak19@gmail.com>
2021-07-06 23:56:17 +0530

    added comments and documentation

--- server.js
+++ server.js
@@ -1,88 +1,108 @@
+//This server accepts posts requests from the clients pinged by websocket server
+//and saves in MongoDB database  
+
 var express = require('express')
 var app = express();
-app.use(express.json());
 var mongo = require('mongodb');
 var MongoClient = mongo.MongoClient;
 
-var db = undefined;
+app.use(express.json());
 
+var db = undefined;
+const PORT= 5000; 
+
+
+
+//Creates and start a mongoDB server in the local host.
+//On succesful connections creates different collections to store 
+//the incoming Clients data based on their type(ex  RAM , CPU usage etc.)   
+//on subsequent connection appends the data rather than overwriting
 MongoClient.connect('mongodb://localhost:27017/',function(err, database){  
       if(err) 
         console.log(err);
       else
       {
-        console.log('Mongo Conn....');
+
+        console.log('Mongo Server Connected...');
+        //create database osInfo
         db = database.db("osInfo");
-
+        
+        console.log('Creating Collections... ');
+        //create collection in database to store CPU usage info
         db.createCollection("cpu", function(err, res) {
-            console.log("cpu created!");
+            console.log("CPU created!");
         });
 
+
+        //create collection in database to store Disk usage info
         db.createCollection("disk", function(err, res) {
-            console.log("disk created!");
+            console.log("Disk created!");
         });
 
+
+        //create collection in database to store CPU usage info
         db.createCollection("ram", function(err, res) {
-            console.log("ram created!");
+            console.log("RAM created!");
         });
 
+
+        //create collection in database to store No. of process running info
         db.createCollection("process", function(err, res) {
-            console.log("process created!");
+            console.log("Process created!");
         });
 
+
+        //create collection in database to store Directory info
         db.createCollection("dir", function(err, res) {
-            console.log("dir created!");
+            console.log("Directory created!");
         });
 
       }
     });
-// //for local server 
-// //in local server DBPASSWOAD and DBusername not required
-// MongoClient.connect('mongodb://'+DATABASEHOST+':'+DATABASEPORT+'/'+DATABASENAME,function(err, db){  
-//       if(err) 
-//         console.log(err);
-//       else
-//       {
 
-//         console.log('Mongo Conn....');
-
-//       }
-//     });
-
+//function to insert into data into database and in their respective 
+//collection based on the type recieved from client
 function insert(type, data) {
     db.collection(type).insertOne(data, function(err, res) {
         if (err) throw err;
-        console.log("1 document inserted of type " + type + 'from userId ' + data.userId);
+        //if succesfully inserted log the details of data and client
+        console.log("Document inserted of type " + type + ' from userId ' + data.userId);
     });
 }
 
+//using express to write into database based on data type and respective URLs
 
+//inserting data into disk collection
 app.post("/disk" , function(req ,res ){
     insert(req.body.type, {userId : req.body.id, data : req.body.info});
     console.log(req.body);
 })
 
+//inserting data into Cpu collection
 app.post("/cpuusage" , function(req ,res ){
     insert(req.body.type, {userId : req.body.id, data : req.body.info});
     console.log(req.body);
 })
 
+//inserting data into Ram collection
 app.post("/ramusage" , function(req ,res ){
     insert(req.body.type, {userId : req.body.id, data : req.body.info});
     console.log(req.body);
 })
 
+//inserting data into process collection
 app.post("/process" , function(req ,res ){
     insert(req.body.type, {userId : req.body.id, data : req.body.info});
     console.log(req.body);
 })
 
+//inserting data into Directory collection
 app.post("/dir" , function(req ,res ){
     insert(req.body.type, {userId : req.body.id, data : req.body.info});
     console.log(req.body);
 })
 
-const PORT= 5000; 
+//create a server and listen on port 5000
 var server = app.listen(PORT , function(){
     console.log("Server listening on port " + PORT);
 })
--- webSocketServer.js
+++ webSocketServer.js
@@ -1,18 +1,34 @@
-var WebSocket = require('ws');
+//Websocket server to connect to all the incoming clients as the number of sockets
+//in a server can be in 10,000's therefore clients simultaneously connected to a single server
+//can be also in the same numbers
+//This server also does not accept any data except for logging purpose
+//Thus websocket server can be scaled up for a large no of users  
 
-const PORT = 3000;
+const WebSocket = require('ws');
+// port no. for websocket connection to client
+const PORT = 3000; 
+
+// Time variable pings all the clients at 1 minutes
 const TIME = 1 * 60 * 1000;
+
+//Creating a websocket server on port 3000
 const wsServer = new WebSocket.Server({port : PORT});
 
-
+//opens websocket connection for server side
 wsServer.on("connection" , function(socket){
+    
     console.log("Connections Established");
+
+    //whenver a clients sends a message logs it
     socket.on("message" , function(msg){
         console.log(msg);
-    }); 
+    });
+    
+    //calls ping functions to echo to all clients after n minutes 
     wsServer.timer = setInterval(function(){ping();} , TIME);
 });
 
+//function to ping all the clients currently connected to server
 function ping() {
     wsServer.clients.forEach(function(client){
         client.ping();
